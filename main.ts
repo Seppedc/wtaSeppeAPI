9a4db2eecc1501980823a1d151475ff71dafd165
Seppedc <60157548+Seppedc@users.noreply.github.com>
2023-06-08 17:23:38 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,25 +2,29 @@ import { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 import data from "./data.json" assert { type: "json" };
 
+const MAX_ITEMS = 5; 
+
 const router = new Router();
 router
   .get("/", (context) => {
-    context.response.body = "Welcome to dinosaur API!";
+    context.response.body = "test api";
   })
   .get("/api", (context) => {
     context.response.body = data;
   })
-  .get("/api/:dinosaur", (context) => {
-    if (context?.params?.dinosaur) {
-      const found = data.find((item) =>
-        item.name.toLowerCase() === context.params.dinosaur.toLowerCase()
-      );
-      if (found) {
-        context.response.body = found;
-      } else {
-        context.response.body = "No dinosaurs found.";
-      }
+  //code for api/edit has been rewriten by chatgpt to fix errors 
+  .post("/api/edit", async (context) => {
+    const { name, points } = await context.request.body().value;
+    const newItem = { name: String(name), score: String(points) };
+    data.push(newItem);
+    data.sort((a: { score: string }, b: { score: string }) => Number(b.score) - Number(a.score));
+    if (data.length > MAX_ITEMS) {
+      data.length = MAX_ITEMS;
     }
+    Deno.writeTextFileSync('./data.json', JSON.stringify(data, null, 2), { append: false });
+
+
+    context.response.body = { success: true, message: "Data updated successfully" };
   });
 
 const app = new Application();
